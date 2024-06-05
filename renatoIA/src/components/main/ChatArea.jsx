1af2eb97a6d94f78486acc8673382c1361504571
hugobatista27 <hugobatista2710@gmail.com>
2024-06-05 11:39:05 -0300

    upda: adiciona novos componentes

--- renatoIA/src/components/main/ChatArea.jsx
+++ renatoIA/src/components/main/ChatArea.jsx
@@ -1,5 +1,8 @@
+import './chatArea.css';
 import { useState } from "react";
 import { WelcomeMessage } from "./welcomeMessage/WelcomeMessage";
+import { SendMessage } from "./sendMessage/SendMessage";
+import { NewProjectButton } from "../sideBar/components/createNewProject/NewProjectButton";
 
 export const ChatArea = () => {
     // project sera o objeto que contem os dados de projetos anteriores do usuario. Enquanto o usuario nao selecionar o projeto ou criar um novo uma mensagem de boas vindas sera exibida com algumas sugestoes de pergunta para serem feitas para a ia
@@ -7,10 +10,11 @@ export const ChatArea = () => {
     const [project, setProject] = useState();
 
     return (
-        <div style={{width: '920px'}}>
+        <div className="chat-area" style={{width: '920px'}}>
             {project ? <div>ChatArea</div> : <WelcomeMessage/>
             }
-            
+            <NewProjectButton isExpanded={true}/>
+            <SendMessage/>
         </div>
     )
 }
