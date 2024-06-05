64c0d9267e6cd7cdbe3eee53d12745231972059d
hugobatista27 <hugobatista2710@gmail.com>
2024-06-05 11:02:48 -0300

    feat: insere os componentes criados

--- renatoIA/src/App.jsx
+++ renatoIA/src/App.jsx
@@ -1,15 +1,19 @@
 import './App.css'
 import { Header } from './components/header/Header'
 import { SideBar } from './components/sideBar/SideBar'
+import { ChatArea } from './components/main/ChatArea';
 
 function App() {
 
   return (
     <main>
       <SideBar/>
-      <div>
+      <div className='full-width'>
         <Header/>
-
+        <div style={{display: 'flex'}}>
+          <ChatArea/>
+          <div>plano de negocio</div>
+        </div>
       </div>
     </main>
   )
