250da40763eabf46681d3789b2d0ff532bfb757f
hugobatista27 <hugobatista2710@gmail.com>
2024-06-05 11:01:30 -0300

    feat: cria e formata o componente ChatArea

---
+++ renatoIA/src/components/main/ChatArea.jsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import { WelcomeMessage } from "./welcomeMessage/WelcomeMessage";
+
+export const ChatArea = () => {
+    // project sera o objeto que contem os dados de projetos anteriores do usuario. Enquanto o usuario nao selecionar o projeto ou criar um novo uma mensagem de boas vindas sera exibida com algumas sugestoes de pergunta para serem feitas para a ia
+
+    const [project, setProject] = useState();
+
+    return (
+        <div style={{width: '920px'}}>
+            {project ? <div>ChatArea</div> : <WelcomeMessage/>
+            }
+        </div>
+    )
+}
