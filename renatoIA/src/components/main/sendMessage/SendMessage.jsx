3b189b813c299d86aa81991ffab89fea03f9c070
hugobatista27 <hugobatista2710@gmail.com>
2024-06-05 11:38:37 -0300

    feat: cria e formata o componente SendMessage

---
+++ renatoIA/src/components/main/sendMessage/SendMessage.jsx
@@ -0,0 +1,12 @@
+import './sendMessage.css';
+
+export const SendMessage = () => {
+    // criar a funcao ded enviar texto e recerber a resposta
+    
+    return (
+        <div className='sendMessage'>
+            <input type="text" name="" id="" placeholder='Digite sua mensagem ou pergunta.'/>
+            <img src="./asd" alt="microphone" />
+        </div>
+    )
+}
