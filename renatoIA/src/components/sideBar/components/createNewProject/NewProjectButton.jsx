7672732efe6eaea3f5885e5929bd2818a7c5bbfb
hugobatista27 <hugobatista2710@gmail.com>
2024-06-08 19:29:06 -0300

    feat: cria a função de criar novos projetos

--- renatoIA/src/components/sideBar/components/createNewProject/NewProjectButton.jsx
+++ renatoIA/src/components/sideBar/components/createNewProject/NewProjectButton.jsx
@@ -1,14 +1,34 @@
+import { useContext } from 'react';
 import './newProjectButton.css'
 import PropTypes from 'prop-types';
+import { ProjectContext } from '../../../../App';
 
 export const NewProjectButton = ({isExpanded}) => {
     NewProjectButton.propTypes = {
         isExpanded: PropTypes.bool.isRequired
     }
 
+    const {projects, setProjects, setSelectedProject} = useContext(ProjectContext)
+
+    const createNewProject = () => {
+        setProjects([...projects, {
+            id: projects[projects.length - 1].index,
+            name: 'new project',
+            proposal: {
+                area: '',
+                service: '',
+            },
+            chat: []
+        }])
+
+        setSelectedProject(projects[projects.length - 1])
+
+        console.log(projects);
+    }
+
     return (
-        <button className='newProject'>
-            <img src="../../../../../public/sideBar/plusicon.png" alt="plus icon" />
+        <button className='newProject' onClick={createNewProject}>
+            <img src="/sideBar/plusicon.png" alt="plus icon" />
             <p className={isExpanded ? 'expanded' : 'closed'}>Criar novo projeto</p>
         </button>
     )
