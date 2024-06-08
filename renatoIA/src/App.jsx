29321a626798368568a5593c4f777324b9bf0066
hugobatista27 <hugobatista2710@gmail.com>
2024-06-08 19:27:10 -0300

    feat: cria o contexto

--- renatoIA/src/App.jsx
+++ renatoIA/src/App.jsx
@@ -1,23 +1,40 @@
 import './App.css'
+import { projects as userProjects } from '../db/data';
 import { Header } from './components/header/Header'
 import { SideBar } from './components/sideBar/SideBar'
 import { ChatArea } from './components/main/ChatArea';
 import { BusinessPlanArea } from './components/main/BusinessPlanArea';
+import { createContext, useContext, useState } from 'react';
+
+const ProjectContext = createContext(null);
 
 function App() {
+  const [projects, setProjects] = useState(userProjects)
+  const [selectedProject, setSelectedProject] = useState()
+
+
+  const contextValues = {
+    projects,
+    setProjects,
+    selectedProject,
+    setSelectedProject
+  }
 
   return (
-    <main>
-      <SideBar/>
-      <div className='full-width' style={{display: 'flex', flexDirection: 'column'}}>
-        <Header/>
-        <div className='content'>
-          <ChatArea/>
-          <BusinessPlanArea/>
+    <ProjectContext.Provider value={contextValues}>
+      <main>
+        <SideBar/>
+        <div className='full-width' style={{display: 'flex', flexDirection: 'column'}}>
+          <Header/>
+          <div className='content'>
+            <ChatArea/>
+            <BusinessPlanArea/>
+          </div>
         </div>
-      </div>
-    </main>
+      </main>
+    </ProjectContext.Provider>
   )
 }
 
+export { ProjectContext }
 export default App
