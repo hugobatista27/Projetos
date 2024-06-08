4e04410aa9982d8e2fbe7046f38de24a88ed542b
hugobatista27 <hugobatista2710@gmail.com>
2024-06-08 19:28:07 -0300

    upda: adiciona o contexto para o componente

--- renatoIA/src/components/main/ChatArea.jsx
+++ renatoIA/src/components/main/ChatArea.jsx
@@ -1,19 +1,18 @@
 import './chatArea.css';
-import { useState } from "react";
+import { useContext } from "react";
 import { WelcomeMessage } from "./welcomeMessage/WelcomeMessage";
 import { SendMessage } from "./sendMessage/SendMessage";
 import { NewProjectButton } from "../sideBar/components/createNewProject/NewProjectButton";
+import { ProjectContext } from '../../App';
 
 export const ChatArea = () => {
-    // project sera o objeto que contem os dados de projetos anteriores do usuario. Enquanto o usuario nao selecionar o projeto ou criar um novo uma mensagem de boas vindas sera exibida com algumas sugestoes de pergunta para serem feitas para a ia
-
-    const [project, setProject] = useState();
+    const { selectedProject } = useContext(ProjectContext)
 
     return (
         <div style={{display:'flex', flexGrow: '1', justifyContent:'center'}}>
             <div className="chat-area">
                 <div>
-                    {project ? <div>ChatArea</div> : <WelcomeMessage/>
+                    {selectedProject ? <div>{selectedProject.name}</div> : <WelcomeMessage/>
                     }
                     <NewProjectButton isExpanded={true}/>
                 </div>
