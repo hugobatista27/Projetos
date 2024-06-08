b644e5f9d77d7cb85e003c525d2a0d415a130f2a
hugobatista27 <hugobatista2710@gmail.com>
2024-06-08 15:34:57 -0300

    feat: cria o arquivo que servirá como fonte de dados

---
+++ renatoIA/db/data.js
@@ -0,0 +1,16 @@
+const suggestions = [
+    { 
+        message: 'Quais os melhores livros sobre empreendedorismo?',
+        img: '../public/chat/suggestions/book icon.png',
+    },
+    { 
+        message: 'Como melhorar meu plano de negócio? ',
+        img: '../public/chat/suggestions/money icon.png',
+    },
+    { 
+        message: 'Qual a velocidade maxima do Kwid?',
+        img: '../public/chat/suggestions/car icon.png',
+    },
+]
+
+export { suggestions }
