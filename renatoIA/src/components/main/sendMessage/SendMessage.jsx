262e1ef24110b883d85e4877d67828e08e0ca921
hugobatista27 <hugobatista2710@gmail.com>
2024-06-09 16:24:09 -0300

    feat: cria a funcionalidade de enviar mensagem

--- renatoIA/src/components/main/sendMessage/SendMessage.jsx
+++ renatoIA/src/components/main/sendMessage/SendMessage.jsx
@@ -1,11 +1,24 @@
 import './sendMessage.css';
+import { ProjectContext } from '../../../App';
+import { useContext } from 'react';
 
 export const SendMessage = () => {
     // criar a funcao ded enviar texto e recerber a resposta
+    const { selectedProject, projects, setProjects } = useContext(ProjectContext);
+
+    const newMessage = (event) => {
+        console.log(event.key);
+        if (event.key === 'Enter') {
+            let copyProjects = [...projects]
+            copyProjects[selectedProject.id - 1].chat.push(event.target.value)
+            setProjects(copyProjects)
+            event.target.value = ''
+        }
+    }
     
     return (
         <div className='sendMessage'>
-            <input type="text" name="" id="" placeholder='Digite sua mensagem ou pergunta.'/>
+            <input onKeyDown={newMessage} type="text" name="" id="" placeholder='Digite sua mensagem ou pergunta.'/>
             <img src="../../../../public/chat/microphone.svg" alt="microphone" />
         </div>
     )
