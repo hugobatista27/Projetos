3b4aeb6c6695149d726837f0a97453e65738d340
hugobatista27 <hugobatista2710@gmail.com>
2024-06-09 21:02:59 -0300

    feat: cria função ao clicar no logo

--- renatoIA/src/components/header/components/logo/Logo.jsx
+++ renatoIA/src/components/header/components/logo/Logo.jsx
@@ -1,13 +1,21 @@
 import './logo.css'
+import { useContext } from 'react'
+import { ProjectContext } from '../../../../App'
 
 export const Logo = () => {
+  const { setSelectedProject } = useContext(ProjectContext)
+
+  const handleProject = () => {
+    setSelectedProject('')
+  }
+
   return (
     <div className="logo">
         {/* adicionar link para a pagina home */}
-        <a href="#">
+        <button onClick={handleProject}>
             <img src="../../../../../public/header/iconlogo.svg" alt="logo" />
             <h1>RenatoIA</h1>
-        </a>
+        </button>
     </div>
   )
 }
